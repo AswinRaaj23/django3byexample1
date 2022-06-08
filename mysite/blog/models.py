81664e95a58445bfec16de02f44483c4e83a9264
AswinRaaj <aswin@testpress.in>
2022-06-08 14:52:05 +0530

    Add tagging functionality

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -24,6 +25,8 @@ class Post(models.Model):
 
     objects=models.Manager() #default manager
     published=PublishedManager() #custom manager
+
+    tags=TaggableManager()
 
 
     class Meta:
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
 from pathlib import Path
 
+env = environ.Env()
+environ.Env.read_env()
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aswinraaj23@gmail.com'
-EMAIL_HOST_PASSWORD = 'nibqmqkvvkcxybim'
+EMAIL_HOST_PASSWORD = env('PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -27,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-@@!o+5&9o199s$7&pv6qxbi)!fr5s0xr_w5^b58z9qua-k(6t9'
+SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
